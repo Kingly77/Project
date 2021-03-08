c5f629d71fae605cd8f5be8c7151d23708043fe1
Kingly77 <shelbygj97@gmail.com>
2021-03-07 20:05:57 -0800

    weather time api basic implantation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,18 @@
 
-fetch('https://worldtimeapi.org/api/ip').then(data=>{
 
-    return data.json();
+let cityName= 'san jose'
+let key = '555cb8f86a8f9b99085c41f79016fb5a';
 
-}).then(data=>{
+fetch('https://worldtimeapi.org/api/ip').then(data=>data.json()).then(data=>{
 
     console.log(data['datetime']);
     let day = dayjs(data["datetime"]);
-    $('#curtime')[0].textContent = day.format('h:m');
+    $('#curTime')[0].textContent = day.format('hh:mm');
 
+})
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data=>data.json()).then(data=>
+{
+    console.log(data['weather']['0']['main']);
+    $('#weather')[0].textContent = data['weather']['0']['main'];
 })
