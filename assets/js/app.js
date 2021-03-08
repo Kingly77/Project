2b7d89ce40c64efd5a6e2e1194447c9c2e6e9467
Kingly77 <shelbygj97@gmail.com>
2021-03-08 15:42:20 -0800

    fixed merge test and master

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,13 @@ fetch('https://worldtimeapi.org/api/ip').then(data=>data.json()).then(data=>{
 
     console.log(data['datetime']);
     let day = dayjs(data["datetime"]);
-    $('#curTime')[0].textContent = day.format('hh:mm');
+    $('#current-time')[0].textContent = day.format('hh:mm');
 
 })
 
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data=>data.json()).then(data=>
 {
     console.log(data['weather']['0']['main']);
-    $('#weather')[0].textContent = data['weather']['0']['main'];
+    $('#current-weather')[0].textContent = data['weather']['0']['main'];
 })
 
