9c4149a32550aae242f3c99e6585d74780d372f8
Kingly <shelbygj97@gmail.com>
2021-03-07 19:45:30 -0800

    time api added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,12 @@
 
-//document.querySelector('#time').textContent ='why';
+fetch('https://worldtimeapi.org/api/ip').then(data=>{
+
+    return data.json();
+
+}).then(data=>{
+
+    console.log(data['datetime']);
+    let day = dayjs(data["datetime"]);
+    $('#curtime')[0].textContent = day.format('h:m');
+
+})
