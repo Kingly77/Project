5bfb378d6991a8dccaeb9f97206010aa3ca3caf7
Kingly77 <shelbygj97@gmail.com>
2021-03-08 16:35:44 -0800

    search functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,32 @@
-
-let cityName= 'san jose'
+let cityName = 'san jose'
 let key = '555cb8f86a8f9b99085c41f79016fb5a';
 
-fetch('https://worldtimeapi.org/api/ip').then(data=>data.json()).then(data=>{
-
-    console.log(data['datetime']);
-    let day = dayjs(data["datetime"]);
-    $('#current-time')[0].textContent = day.format('hh:mm');
-
-})
-
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data=>data.json()).then(data=>
+function getLocalTime()
 {
-    console.log(data['weather']['0']['main']);
-    $('#current-weather')[0].textContent = data['weather']['0']['main'];
-})
+    fetch('https://worldtimeapi.org/api/ip').then(data => data.json()).then(data =>
+    {
 
+        console.log(data['datetime']);
+        let day = dayjs(data["datetime"]);
+        $('#current-time')[0].textContent = day.format('hh:mm');
+
+    })
+}
+
+function getWeather()
+{
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data => data.json()).then(data =>
+    {
+        console.log(data['weather']['0']['main']);
+        $('#current-weather')[0].textContent = data['weather']['0']['main'];
+    })
+}
+
+getWeather();
+getLocalTime();
+
+$('#lookup').click(() =>
+{
+    cityName = $('#input')[0].value;
+})
