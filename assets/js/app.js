0a8cecdc42e7ecd8d600623c70253bb2baae4205
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-03-09 16:31:31 -0800

    Change weather on click added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,23 @@
 let cityName = 'San Jose'
 let key = '555cb8f86a8f9b99085c41f79016fb5a';
 
-function getLocalTime()
-{
-    fetch('https://worldtimeapi.org/api/ip').then(data => data.json()).then(data =>
-    {
+function getLocalTime() {
+    fetch('https://worldtimeapi.org/api/ip')
+        .then(data => data.json())
+        .then(data => {
+            console.log(data);
 
-        console.log(data['datetime']);
-        let day = dayjs(data["datetime"]);
-        $('#current-time')[0].textContent = day.format('hh:mm');
+            console.log(data['datetime']);
+            let day = dayjs(data["datetime"]);
+            $('#current-time')[0].textContent = day.format('hh:mm');
 
-    })
+        })
 }
 
-function getWeather()
-{
+function getWeather() {
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data => data.json()).then(data =>
-    {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data => data.json()).then(data => {
+
         console.log(data['weather']['0']['main']);
         $('#current-weather')[0].textContent = data['weather']['0']['main'];
     })
@@ -27,8 +27,8 @@ getWeather();
 getLocalTime();
 $(`#cityName`)[0].textContent = `${cityName}`;
 
-$('#lookup').click(() =>
-{
+$('#lookup').click(() => {
     cityName = $('#input')[0].value;
     $(`#cityName`)[0].textContent = `${cityName}`;
+    getWeather(cityName);
 })
