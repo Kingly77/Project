dbc716130b23a6c322cd5bf7b7e9cd5d2a564397
Kingly77 <shelbygj97@gmail.com>
2021-03-10 00:57:43 -0800

    fixed formating

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,13 @@ let key = '555cb8f86a8f9b99085c41f79016fb5a';
 
 dayjs.extend(window.dayjs_plugin_utc)
 
-
-
-function getData() {
-
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data => data.json()).then(data => {
+function getData()
+{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data => data.json()).then(data =>
+    {
 
         $('#current-weather')[0].textContent = data['weather']['0']['main'];
-        $('#current-time')[0].textContent = dayjs.utc().utcOffset(data['timezone']/60).format('hh:mm');
+        $('#current-time')[0].textContent = dayjs.utc().utcOffset(data['timezone'] / 60).format('hh:mm');
 
     })
 }
@@ -19,7 +18,8 @@ getData();
 
 $(`#cityName`)[0].textContent = `${cityName}`;
 
-$('#lookup').click(() => {
+$('#lookup').click(() =>
+{
     cityName = $('#input')[0].value;
     $(`#cityName`)[0].textContent = `${cityName}`;
     getData();
