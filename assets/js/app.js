2172480e9f2f582f179eec15b9c1c3b0119c3e20
Kingly77 <shelbygj97@gmail.com>
2021-03-10 00:37:49 -0800

    Local time and weather functional

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,34 +3,24 @@ let key = '555cb8f86a8f9b99085c41f79016fb5a';
 
 dayjs.extend(window.dayjs_plugin_utc)
 
-function getLocalTime() {
-    fetch('https://worldtimeapi.org/api/timezone/Africa/Ndjamena')
-        .then(data => data.json())
-        .then(data => {
-            console.log(data);
-            let day = dayjs.utc(data["datetime"]);
-            console.log(day);
-            $('#current-time')[0].textContent = day.format('hh:mm');
 
-        })
-}
 
-function getWeather() {
+function getData() {
 
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`).then(data => data.json()).then(data => {
 
-        console.log(data['weather']['0']['main']);
         $('#current-weather')[0].textContent = data['weather']['0']['main'];
+        $('#current-time')[0].textContent = dayjs.utc().utcOffset(data['timezone']/60).format('hh:mm');
+
     })
 }
 
-getWeather();
-getLocalTime();
+getData();
 
 $(`#cityName`)[0].textContent = `${cityName}`;
 
 $('#lookup').click(() => {
     cityName = $('#input')[0].value;
     $(`#cityName`)[0].textContent = `${cityName}`;
-    getWeather(cityName);
+    getData();
 })
